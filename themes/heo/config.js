3d82b0df2922a61c5663cf99ae2c66709a06a015
SiqiXiang <41234783+SiqiXiang@users.noreply.github.com>
2025-09-01 01:54:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,19 +17,18 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '有一道绿光',
+  HEO_HERO_TITLE_2: '幸福在哪里',
+  HERO_TITLE_4: '我正在看',
+  HERO_TITLE_5: '异形：地球',
+  HERO_TITLE_LINK: 'https://www.themoviedb.org/tv/157239-alien-earth',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '无心插柳', url: '/tag/无心插柳' },
+  HEO_HERO_CATEGORY_2: { title: '提桶跑路', url: '/tag/提桶跑路' },
+  HEO_HERO_CATEGORY_3: { title: '技术归档', url: '/tag/技术归档' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
