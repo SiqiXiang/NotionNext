4bebb3784089cc801c5d5bf3d3a566246538b2ec
SiqiXiang <41234783+SiqiXiang@users.noreply.github.com>
2026-05-05 00:09:41 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -64,17 +64,15 @@ const Player = () => {
       <link
         rel='stylesheet'
         type='text/css'
-        href='https://cdn.jsdelivr.net/npm/aplayer@1.10.0/dist/APlayer.min.css'
+        href='https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.css'
       />
       {meting ? (
         <meting-js
           fixed='true'
           type='playlist'
           preload='auto'
-          api={siteConfig(
-            'MUSIC_PLAYER_METING_API',
-            'https://api.i-meto.com/meting/api?server=:server&type=:type&id=:id&r=:r'
-          )}
+          lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
+          api='https://api.injahow.cn/meting/?server=tencent&type=playlist&id=9708105470'
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
           server={siteConfig('MUSIC_PLAYER_METING_SERVER')}
